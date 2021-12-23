5d1f2a40a0650206e96c4b7ace255f2a0a8549fb
Sirius <curiosity@Siriuss-MacBook-Pro.local>
2021-12-23 11:35:37 +0530

    added the down key event listner

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+# When you turn off the animation you need to manually
+#Update the screen and refresh it every single time
+#using while loop
+screen.tracer(0)
 
 # adding the paddle to the game
 paddle = Turtle()
@@ -26,9 +30,13 @@ def go_down():
 # set up the paddle listner
 screen.listen()
 screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 
+game_is_on = True
 
+while game_is_on:
+    screen.update() # show every thing which happened so far
 
 
 
