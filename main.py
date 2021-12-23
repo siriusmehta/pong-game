cd272c62fe66041f26d5c8a914c663f77636fb5b
Sirius <curiosity@Siriuss-MacBook-Pro.local>
2021-12-23 11:24:02 +0530

    added the event listner for up and down of the right paddle

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+# create the screen for the Pong game
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+# adding the paddle to the game
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+# set up the paddle listner
+screen.listen()
+screen.onkey(go_up, "Up")
+
+
+
+
+
 
 
 screen.exitonclick()
