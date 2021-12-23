28c21f4f9cbb32cecb9b2f969e08fdcb9e177b0d
Sirius <curiosity@Siriuss-MacBook-Pro.local>
2021-12-23 11:46:43 +0530

    added the left paddle with up and down evebt listner keys

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+
+
 
 # create the screen for the Pong game
 screen = Screen()
@@ -10,27 +13,20 @@ screen.title("Pong")
 #using while loop
 screen.tracer(0)
 
-# adding the paddle to the game
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 
 # set up the paddle listner
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
 
 
 game_is_on = True
